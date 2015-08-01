c04c1d910693364ef796355111ce2d2f14890e1f
lanou <lanou@lanoudeMac-mini-5.local>
2015-08-01 10:50:03 +0800

    Initial Commit

---
+++ 别踩白块游戏/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  别踩白块游戏
+//
+//  Created by lanou on 15/8/1.
+//  Copyright (c) 2015年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
